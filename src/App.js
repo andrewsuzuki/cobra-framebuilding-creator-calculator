8bc4694aca7357851f9d1f56077981bb61f5d568
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2021-03-23 21:10:43 -0400

    Init readme, editorconfig, remove style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './Calculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Creator Frame Fixture Setup Calculator</h1>
       </header>
+      <Calculator />
+      <footer className="App-footer">
+        By <a href="https://andrewsuzuki.com" title="Andrew Suzuki">Andrew Suzuki</a> &middot; <a href="https://github.com/andrewsuzuki/cobra-framebuilding-creator-calculator" title="Source code on GitHub">Source</a>
+      </footer>
     </div>
   );
 }
---
+++ src/Calculator.js
@@ -0,0 +1,17 @@
+
+/**
+ * Input (required): hta, bbdrop, cslength, htlength, stack, reach
+ * Optional:
+ * - sta (for ST-HT compat check)
+ * 
+ * Checks:
+ * - TODO
+ */
+
+function Calculator() {
+  return (
+    <div><form>Stack: <input type="text" /></form></div>
+  );
+}
+
+export default Calculator;
