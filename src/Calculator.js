dbad14545afec38d7c1bf805aaf6bd4bba980ed1
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2021-03-24 23:59:27 -0400

    Remaining result validators

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,26 +2,25 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import classNames from "classnames";
+import { useEffect } from "react";
 
-/**
- * Input (required): hta
- * Input (sta-hta): sta
- * Input (dax/day): bbdrop, cslength
- * Input (htx/hty/htytop): stack, reach, htlength
- *
- * Outputs/checks:
- * - htx
- * - hty
- * - htytop
- * - dax
- * - day
- * - sta-hta
- *
- * Output statuses:
- * - Ok (+value)
- * - Waiting
- * - Doesn't fit
- */
+// Constants
+// Limits are inclusive unless noted.
+const MIN_ST_HT_ANGLE = -10;
+const MAX_ST_HT_ANGLE = 10;
+const MIN_HTX = 80;
+const MAX_HTX = 700;
+const MIN_HTY = 0;
+const MAX_HTY = 400; // (accounting for upper cone standoff as well)
+const MIN_HT_LENGTH_LARGEST = 60; // minimum ht length for the largest reasonable ht diameter
+const HT_CONE_RANGE = 20; // y difference from smallest to largest reasonable ht diameter
+const MIN_DAX = 50;
+const MAX_DAX = 600;
+const MIN_DAY = 0;
+const MAX_DAY = 250;
+// Derived
+const MAX_HTY_TOP_LARGEST = MAX_HTY + MIN_HT_LENGTH_LARGEST; // maximum htytop of the widest reasonable ht
+const MAX_HTY_TOP_SMALLEST = MAX_HTY + MIN_HT_LENGTH_LARGEST - HT_CONE_RANGE; // maximum htytop of the narrowest reasonable ht
 
 // helper for yup transform function
 function emptyStringToNull(value, originalValue) {
@@ -30,6 +29,24 @@ function emptyStringToNull(value, originalValue) {
   }
   return value;
 }
+
+// additional method to ensure abs(bbdrop) is less than or equal to cslength
+yup.addMethod(yup.number, "absMax", function (
+  max,
+  // eslint-disable-next-line no-template-curly-in-string
+  message = "${path} must be less than or equal to the absolute value of ${max}"
+) {
+  return this.test({
+    message,
+    name: "max",
+    exclusive: true,
+    params: { max },
+    test(value) {
+      const other = this.resolve(max);
+      return value == null || other == null || Math.abs(value) <= other;
+    },
+  });
+});
 
 const basicInputSchema = yup.object().shape({
   hta: yup
@@ -44,11 +61,19 @@ const basicInputSchema = yup.object().shape({
     .lessThan(180)
     .transform(emptyStringToNull)
     .nullable(),
-  bbdrop: yup.number().transform(emptyStringToNull).nullable(),
+  bbdrop: yup
+    .number()
+    .transform(emptyStringToNull)
+    .nullable()
+    .absMax(yup.ref("cslength")),
   cslength: yup.number().moreThan(0).transform(emptyStringToNull).nullable(),
   stack: yup.number().moreThan(0).transform(emptyStringToNull).nullable(),
   reach: yup.number().moreThan(0).transform(emptyStringToNull).nullable(),
-  htlength: yup.number().moreThan(0).transform(emptyStringToNull).nullable(),
+  htlength: yup
+    .number()
+    .min(MIN_HT_LENGTH_LARGEST)
+    .transform(emptyStringToNull)
+    .nullable(),
 });
 
 const labels = {
@@ -119,7 +144,9 @@ function Result({ label, errors, values, calculate, validate }) {
             <>
               {result}&nbsp;
               {bad ? (
-                <span className="text-error">✗ Out of range</span>
+                <span className="text-error">
+                  ✗ {bad !== true ? bad : "Out of range"}
+                </span>
               ) : (
                 <span className="text-success">✓ Ok</span>
               )}
@@ -136,10 +163,21 @@ function degToRad(degrees) {
 }
 
 function Calculator() {
-  const { register, watch, errors } = useForm({
-    mode: "onTouched",
+  const { register, watch, errors, trigger } = useForm({
+    mode: "all",
     resolver: yupResolver(basicInputSchema),
   });
+
+  // Trigger bbdrop validation when cslength (dependency) changes
+  const cslength = watch("cslength");
+  useEffect(() => {
+    trigger("bbdrop");
+  }, [trigger, cslength]);
+
+  const htlength = watch("htlength");
+
+  const valuesMap = (...fields) =>
+    fields.reduce((acc, field) => ({ [field]: watch(field), ...acc }), {});
 
   return (
     <form>
@@ -192,7 +230,7 @@ function Calculator() {
         <div className="col-6">
           <InputField
             name="bbdrop"
-            errorMessage="Must be a number"
+            errorMessage="Must be a number that is less than or equal to CS length in magnitude"
             errors={errors}
             register={register}
           />
@@ -202,7 +240,7 @@ function Calculator() {
         <div className="col-6">
           <InputField
             name="htlength"
-            errorMessage="Must be a positive number"
+            errorMessage={`Must be a positive number greater than or equal to ${MIN_HT_LENGTH_LARGEST}`}
             errors={errors}
             register={register}
           />
@@ -212,9 +250,11 @@ function Calculator() {
       <Result
         label="ST-HT angle"
         errors={errors}
-        values={{ hta: watch("hta"), sta: watch("sta") }}
+        values={valuesMap("hta", "sta")}
         calculate={({ hta, sta }) => sta - hta}
-        validate={(v) => (v < -10 ? true : v > 10 ? true : false)}
+        validate={(v) =>
+          v < MIN_ST_HT_ANGLE ? true : v > MAX_ST_HT_ANGLE ? true : false
+        }
       />
       <Result
         label={
@@ -223,16 +263,12 @@ function Calculator() {
           </>
         }
         errors={errors}
-        values={{
-          hta: watch("hta"),
-          stack: watch("stack"),
-          reach: watch("reach"),
-        }}
+        values={valuesMap("hta", "stack", "reach")}
         calculate={({ hta, stack, reach }) =>
-          Math.sqrt(Math.pow(stack, 2), Math.pow(reach, 2)) *
+          Math.sqrt(Math.pow(stack, 2) + Math.pow(reach, 2)) *
           Math.sin(degToRad(180) - degToRad(hta) - Math.atan(stack / reach))
         }
-        validate={(v) => false} // TODO
+        validate={(v) => v < MIN_HTX || v > MAX_HTX}
       />
       <Result
         label={
@@ -241,18 +277,29 @@ function Calculator() {
           </>
         }
         errors={errors}
-        values={{
-          hta: watch("hta"),
-          stack: watch("stack"),
-          reach: watch("reach"),
-          htlength: watch("htlength"),
-        }}
+        values={valuesMap("hta", "stack", "reach", "htlength")}
         calculate={({ hta, stack, reach, htlength }) =>
-          Math.sqrt(Math.pow(stack, 2), Math.pow(reach, 2)) *
+          Math.sqrt(Math.pow(stack, 2) + Math.pow(reach, 2)) *
             Math.cos(degToRad(180) - degToRad(hta) - Math.atan(stack / reach)) -
           htlength
         }
-        validate={(v) => false} // TODO
+        validate={(v) => {
+          if (v < MIN_HTY || v > MAX_HTY) {
+            return true;
+          }
+          const htytop = v + parseFloat(htlength);
+          if (htytop > MAX_HTY_TOP_SMALLEST) {
+            const impossible = htytop > MAX_HTY_TOP_LARGEST;
+            return (
+              <>
+                HT<sub>Y</sub> (bottom) is within range, but{" "}
+                {impossible
+                  ? "the top of the head tube will exceed the upper limit"
+                  : "the top of the head tube could possibly exceed the upper limit, depending on its diameter"}
+              </>
+            );
+          }
+        }}
       />
       <Result
         label={
@@ -261,16 +308,12 @@ function Calculator() {
           </>
         }
         errors={errors}
-        values={{
-          hta: watch("hta"),
-          cslength: watch("cslength"),
-          bbdrop: watch("bbdrop"),
-        }}
+        values={valuesMap("hta", "cslength", "bbdrop")}
         calculate={({ hta, cslength, bbdrop }) =>
           cslength *
-          Math.cos(degToRad(90) - degToRad(hta) - Math.asin(bbdrop / cslength))
+          Math.cos(degToRad(90) - degToRad(hta) + Math.asin(bbdrop / cslength))
         }
-        validate={(v) => false} // TODO
+        validate={(v) => v < MIN_DAX || v > MAX_DAX}
       />
       <Result
         label={
@@ -279,16 +322,12 @@ function Calculator() {
           </>
         }
         errors={errors}
-        values={{
-          hta: watch("hta"),
-          cslength: watch("cslength"),
-          bbdrop: watch("bbdrop"),
-        }}
+        values={valuesMap("hta", "cslength", "bbdrop")}
         calculate={({ hta, cslength, bbdrop }) =>
           cslength *
-          Math.sin(degToRad(90) - degToRad(hta) - Math.asin(bbdrop / cslength))
+          Math.sin(degToRad(90) - degToRad(hta) + Math.asin(bbdrop / cslength))
         }
-        validate={(v) => false} // TODO
+        validate={(v) => v < MIN_DAY || v > MAX_DAY}
       />
     </form>
   );
